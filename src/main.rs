f3acc5481b705da731114a3e4873dc82afd77b2c
Shahariar Munir <skssmd78475@gmail.com>
2025-12-27 12:04:20 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use axum::{extract::State, routing::post, Json, Router};
 use serde::Deserialize;
-use serde_json::json;
+
 use std::{collections::HashMap, env, sync::Arc};
 use tokio::process::Command;
 use tracing::{info, error, warn, debug, instrument};
@@ -71,7 +71,7 @@ async fn handle_deploy(
 ) -> &'static str {
     info!("📥 Webhook request received");
 
-    println!(payload);
+    debug!("Payload received: {:?}", payload);
     // 1. Lookup Project Path
     let project_path = match state.config.get(&payload.project) {
         Some(path) => {
